b66edaa78d30ccec81563973f145dc44913f1e50
raiden808 <raiden.nalasa@gmail.com>
2020-01-24 05:26:47 +0800

    added utils

---
+++ app.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const http        =   require('http')
+const express     =   require('express')
+const app         =   express()
+const bodyParser  =   require('body-parser') 
+const cors        =   require('cors')
+const morgan      =   require('morgan')
+
+/**
+ * DB connection
+ */
+const blogRouters  = require('./controllers/blogs')
+
+
+/**
+ * Extend morgan request logger
+ */
+morgan.token('person', (request) => {
+	if(request.method !== 'GET'){
+		return JSON.stringify(request.body)
+	}
+})
+
+/**
+ * Morgan terminal logs
+ */
+app.use(
+	morgan(':method :url :status :res[content-length] - :response-time ms :person')
+)
+
+/**
+ * Enables cross origin policy
+ */
+app.use(cors())
+
+/**
+ * Enable request.body access  
+ */
+app.use(bodyParser.json())
+
+/**
+ * Base url for api
+ */
+app.use('/api/blogs', blogRouters)
+
+
+module.exports = app
+
--- index.js
+++ index.js
@@ -1,51 +1,12 @@
-require('dotenv').config()
-const http        =   require('http')
-const express     =   require('express')
-const app         =   express()
-const bodyParser  =   require('body-parser') 
-const cors        =   require('cors')
-const morgan      =   require('morgan')
-
 /**
- * DB connection
+ * Primary file
  */
-const blogRouters  = require('./controllers/blogs')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
 
+const server = http.createServer(app)
 
-/**
- * Extend morgan request logger
- */
-morgan.token('person', (request) => {
-	if(request.method !== 'GET'){
-		return JSON.stringify(request.body)
-	}
-})
-
-/**
- * Morgan terminal logs
- */
-app.use(
-	morgan(':method :url :status :res[content-length] - :response-time ms :person')
-)
-
-/**
- * Enables cross origin policy
- */
-app.use(cors())
-
-/**
- * Enable request.body access  
- */
-app.use(bodyParser.json())
-
-/**
- * Base url for api
- */
-app.use('/api/blogs', blogRouters)
-
-
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, () =>{
-	console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT,() => {
+    console.log(`Server running on port ${config.PORT}`)
 })
