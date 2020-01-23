3dd0001a5a7100481ba3a4aea72e5f2ccde77705
raiden808 <raiden.nalasa@gmail.com>
2020-01-23 12:45:33 +0800

    refactor template

---
+++ models/blog.js
@@ -0,0 +1,36 @@
+/**
+ * MongoDB database schema
+ */
+const mongoose = require('mongoose')
+
+mongoose.set('useFindAndModify', false)
+
+/**
+ * base url
+ */
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+/**
+ * Connection verification
+ */
+mongoose.connect(url, { useNewUrlParser:true, useUnifiedTopology: true })
+	.then(()=>{
+		console.log('Connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB', error.message)
+	})
+    
+/**
+ * MongoDB document structure
+ */
+const blogSchema = mongoose.Schema({
+	title : String,
+	author : String,
+	url : String,
+	likes : Number
+})
+
+module.exports = mongoose.model('Note', blogSchema)
