86c0262605a9f66121f6dc0751161d2d33d7b129
raiden808 <raiden.nalasa@gmail.com>
2020-01-28 07:01:09 +0800

    total likes added

---
+++ tests/list_helper.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+	const blogs = []
+
+	const result = listHelper.dummy(blogs)
+	expect(result).toBe(1)
+})
+
+describe('Total likes', () => {
+	const listWithOneBlog  = [
+		{
+			'_id': '5e291dbc46accf375cf6a23d',
+			'title': 'Test Blog',
+			'url': 'google.com',
+			'likes': 5,
+			'__v': 0
+		},
+		{
+			'_id': '5e291dd046accf375cf6a23e',
+			'title': 'Test Blog',
+			'author': 'Bran De Ipille',
+			'url': 'google.com',
+			'likes': 0,
+			'__v': 0
+		},
+	]
+
+	test('When list has only one blog equals the likes of that', () => {
+		const result = listHelper.totalLikes(listWithOneBlog)
+		expect(result).toBe(5)
+	})
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = () => {
 	return 1
 }
 
+const totalLikes = (blogs) =>{
+	return blogs.reduce((total, blog) => { return total + blog.likes}, 0)
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
