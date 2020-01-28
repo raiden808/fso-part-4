d7c09d3e9e31431dd6f618fd64ad87763a41b661
raiden808 <raiden.nalasa@gmail.com>
2020-01-28 08:11:17 +0800

    update docs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,11 +6,37 @@ const dummy = () => {
 	return 1
 }
 
+/**
+ * Display total likes
+ * @method
+ * @param {object} blogs - blog list
+ */
 const totalLikes = (blogs) =>{
 	return blogs.reduce((total, blog) => { return total + blog.likes}, 0)
 }
 
+/**
+ * Return highest likes
+ * @method
+ * @param {object} blogs - blog list
+ */
+const favoriteBlog = (blogs) => {
+
+	/**
+     * retrieves largest value
+     */
+	var max = blogs.reduce(function(a, b) {
+		return Math.max(a, b.likes)
+	},0)
+    
+	/**
+     * finds and retrieve largst value in an object.
+     */
+	return blogs.find(element => element.likes == max)
+}
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
