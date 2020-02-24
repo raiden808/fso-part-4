7ad050bf4077836b96587f6ead9fcc0b9b8eae7f
raiden808 <raiden.nalasa@gmail.com>
2020-02-24 08:32:02 +0800

    establish test env

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,13 @@ require('dotenv').config()
 let PORT        = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
+/**
+ * Use test environment when Jest it used
+ */
+if(process.env.NODE_ENV === 'test'){
+	MONGODB_URI = process.env.TEST_MONGODB_URI
+}
+
 module.exports = {
 	MONGODB_URI,
 	PORT
